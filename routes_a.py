7eb84a14ca02371540801e83bc81576c9d706aba
Fatima Salić <fatima.salic@fet.ba>
2026-05-08 13:52:22 +0200

    Implementirana POST metoda za kreiranje novih resursa

--- routes_a.py
+++ routes_a.py
@@ -6,7 +6,7 @@ from models_a import Director, DirectorCreate, DirectorUpdate
 
 router = APIRouter(prefix="/directors", tags=["Directors"])
 
-# Dohvatanje liste svih reditelja (sa opcionalnim filterom po nacionalnosti)
+# Dohvatanje liste svih režisera (sa opcionalnim filterom po nacionalnosti)
 @router.get("/")
 def get_directors(
     nationality:Optional[str]=Query(default=None),
@@ -20,9 +20,19 @@ def get_directors(
     directors = session.exec(query).all()
     return directors
 
+# Dohvatanje jednog režisera po ID-u
 @router.get("/{director_id}")
 def get_director(director_id: int, session: Session = Depends(get_session)):
     director = session.get(Director, director_id)
     if not director:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Reditelj nije pronađen")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Režiser nije pronađen")
     return director
+
+# Kreiranje novog reditelja
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_director(director: DirectorCreate, session: Session = Depends(get_session)):
+    new_director = Director.model_validate(director)
+    session.add(new_director)
+    session.commit()
+    session.refresh(new_director)
+    return new_director
