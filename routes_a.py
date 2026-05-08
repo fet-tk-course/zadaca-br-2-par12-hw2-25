07af2439d4ed2a2d0c245b1b17954884fef96267
Fatima Salić <fatima.salic@fet.ba>
2026-05-08 13:40:59 +0200

    Implementiran GET /directors/{director_id} za dohvaćanje pojedinačnog resursa

--- routes_a.py
+++ routes_a.py
@@ -6,6 +6,7 @@ from models_a import Director, DirectorCreate, DirectorUpdate
 
 router = APIRouter(prefix="/directors", tags=["Directors"])
 
+# Dohvatanje liste svih reditelja (sa opcionalnim filterom po nacionalnosti)
 @router.get("/")
 def get_directors(
     nationality:Optional[str]=Query(default=None),
@@ -18,3 +19,10 @@ def get_directors(
 
     directors = session.exec(query).all()
     return directors
+
+@router.get("/{director_id}")
+def get_director(director_id: int, session: Session = Depends(get_session)):
+    director = session.get(Director, director_id)
+    if not director:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Reditelj nije pronađen")
+    return director
