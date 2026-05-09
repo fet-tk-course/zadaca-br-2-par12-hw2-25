13279f8e12e7738e8404bbe9abf8d058fb659d6c
Elma <elma.donlic@fet.ba>
2026-05-09 22:30:03 +0200

    fix: rijesen konflikt u main.py (Student B)

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from database import create_db_and_tables
-<<<<<<< HEAD
 
 from routes_b import router as movie_router
-=======
 from routes_a import router as director_router
->>>>>>> student-A-branch
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,11 +18,8 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-<<<<<<< HEAD
 app.include_router(movie_router)
-=======
 app.include_router(director_router)
->>>>>>> student-A-branch
 
 @app.get("/")
 def read_root():
